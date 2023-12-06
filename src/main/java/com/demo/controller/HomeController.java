21871836d9d606173ce8110afa1562c692310a03
ziya <koltikin210@gmail.com>
2023-12-07 00:07:15 +0300

    Initial commit

---
+++ src/main/java/com/demo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/home")
+public class HomeController {
+    @GetMapping
+    public ResponseEntity<String> getHomePage(){
+        return ResponseEntity.ok().body(
+                "welcome to Api End point"
+        );
+    }
+}
